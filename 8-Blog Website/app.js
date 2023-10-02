1d749f92cf7f853ee804502cfb9a8033e57102d6
suvam <suvampravu@gmail.com>
2023-10-02 14:59:05 +0530

    commit the finishing touch

--- 8-Blog Website/app.js
+++ 8-Blog Website/app.js
@@ -1,8 +1,8 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,22 +12,67 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
+let publicPosts = [];
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    homeContent: homeStartingContent,
+    publicPosts: publicPosts
+
+  });
+
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', { aboutContent: aboutContent })
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { contactContent: contactContent })
+})
+
+app.get('/compose', (req, res) => {
+  res.render('compose')
+})
+
+app.post('/compose', (req, res) => {
+  let composeTitle = req.body.title;
+  let composePost = req.body.post;
+
+  let blog = {
+    title: composeTitle,
+    post: composePost
+  }
+
+  publicPosts.push(blog);
 
 
 
+  res.redirect('/')
+})
+
+
+app.get('/post/:topic', (req, res) => {
+  let storeTopic = _.lowerCase(req.params.topic);
+
+  publicPosts.forEach(element => {
+    let storeTitle = _.lowerCase(element.title);
+
+    if (storeTitle === storeTopic) {
+      res.render('post', { blogTitle: element.title, blogPost: element.post });
+      
+    }
+
+  });
+
+})
 
 
 
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
