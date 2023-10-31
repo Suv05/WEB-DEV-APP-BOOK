79bffcdd3790a3948261be963359abfc3becef1e
suvam <suvampravu@gmail.com>
2023-10-31 16:54:11 +0530

    commit my changes

--- 8-Blog Website/app.js
+++ 8-Blog Website/app.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require('lodash');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://suvampravu:zoMZBUgehiq2RcbK@cluster0.xmgiiul.mongodb.net/Blog-website');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,14 +17,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-let publicPosts = [];
+const Ankita = mongoose.model('Ankita', {
+  title: String,
+  blog: String
+});
+
 
 app.get('/', (req, res) => {
-  res.render('home', {
-    homeContent: homeStartingContent,
-    publicPosts: publicPosts
 
-  });
+  Ankita.find({}).exec()
+    .then((blogs) => {
+      res.render('home', { blogs, homeContent: homeStartingContent });
+    }).catch((err) => {
+      console.log(err);
+      res.status(500).send('Internal server Error');
+    })
+
 
 })
 
@@ -43,36 +52,33 @@ app.post('/compose', (req, res) => {
   let composeTitle = req.body.title;
   let composePost = req.body.post;
 
-  let blog = {
-    title: composeTitle,
-    post: composePost
-  }
+  const blog1 = new Ankita({
+    title: `${composeTitle}`,
+    blog: `${composePost}`
 
-  publicPosts.push(blog);
+  });
 
-
+  blog1.save().then(() => {
+    console.log("Saved sucessfuly");
+  });
 
   res.redirect('/')
 })
 
 
-app.get('/post/:topic', (req, res) => {
-  let storeTopic = _.lowerCase(req.params.topic);
+app.get('/post/:id', (req, res) => {
 
-  publicPosts.forEach(element => {
-    let storeTitle = _.lowerCase(element.title);
+  const storeId = req.params.id;
 
-    if (storeTitle === storeTopic) {
-      res.render('post', { blogTitle: element.title, blogPost: element.post });
-      
-    }
+  Ankita.findById(storeId)
+    .then((blogs) => {
+      res.render('post', { blogs });
 
-  });
+    })
 
 })
 
 
-
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
