4357c438fb977d932bfa698720185be2ac67726e
suvam <suvampravu@gmail.com>
2023-11-03 23:47:42 +0530

    wishiper app setup complete

--- 6-Whisper Screcets/app.js
+++ 6-Whisper Screcets/app.js
@@ -1,15 +1,37 @@
 //jshint esversion:6
-const express=require('express');
-const bodyParser=require('body-parser');
-const ejs=require('ejs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
 
-const app=express();
+const app = express();
 
 app.use(express.static('public'));
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://127.0.0.1:27017/UserDB');
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const User = new mongoose.model('User', userSchema)
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
 
 
-app.listen('3000',()=>{
+app.listen('3000', () => {
     console.log("server is runing on port 3000");
 })
