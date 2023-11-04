87e4eef7bd6e4f3b42f0a7f6aeef1162cccb8bb5
suvam <suvampravu@gmail.com>
2023-11-04 22:00:48 +0530

    storing email and password of users

--- 6-Whisper Screcets/app.js
+++ 6-Whisper Screcets/app.js
@@ -31,6 +31,40 @@ app.get('/register', (req, res) => {
     res.render('register')
 })
 
+app.post('/register', (req, res) => {
+    const user1 = new User({
+        email: req.body.username,
+        password: req.body.password,
+    })
+
+    user1.save().then(() => {
+        res.render('secrets')
+    }).catch((err) => {
+        console.log(err);
+    })
+})
+
+app.post('/login', (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({ email: username }).then((foundUser) => {
+        if (foundUser) {
+            // If a user with the given email is found, check their password
+            if (foundUser.password === password) {
+                res.render('secrets');
+            } else {
+                // Password doesn't match
+                res.send('Incorrect password');
+            }
+        } else {
+            // No user with the given email found
+            res.send('User not found');
+        }
+    }).catch((err) => {
+        console.log(err);
+    })
+})
 
 app.listen('3000', () => {
     console.log("server is runing on port 3000");
