e7cae19c9838fc82a9d4b4efab40af303c348e80
suvam <suvampravu@gmail.com>
2024-08-10 00:44:09 +0530

    add custom error handlers

--- 13-Task-Maneger/Controllers/tasks.control.js
+++ 13-Task-Maneger/Controllers/tasks.control.js
@@ -1,78 +1,62 @@
 import mongoose from "mongoose";
 import TaskList from "../models/Task.model.js";
+import { asyncWrapper } from "../Middleware/asyncWarp.js";
+import NotFoundError from "../Error/NotFoundError.js";
+import AppError from "../Error/AppError.js";
 
-export const getAllTasks = async (req, res) => {
-  try {
-    const allTask = await TaskList.find({});
-    res.status(200).json({ msg: "get all tasks", allTask });
-  } catch (error) {
-    res.status(500).json({ msg: "can't get all tasks", error: error.message });
+export const getAllTasks = asyncWrapper(async (req, res, next) => {
+  const allTask = await TaskList.find({});
+  if (!allTask.length) {
+    throw new NotFoundError("No tasks found");
   }
-};
+  res.status(200).json({ msg: "get all tasks", allTask });
+});
 
-export const createTasks = async (req, res) => {
-  try {
-    const task = req.body;
-    const newTask = await TaskList.create({
-      task: task.task,
-    });
-    console.log(newTask);
-    res.status(200).json({ msg: "Task created successfully", newTask });
-  } catch (error) {
-    console.error("Error creating task:", error);
-    res.status(500).json({ msg: "Can't create tasks", error: error.message });
+export const createTasks = asyncWrapper(async (req, res, next) => {
+  const task = req.body;
+  const newTask = await TaskList.create({
+    task: task.task,
+  });
+  res.status(200).json({ msg: "Task created successfully", newTask });
+});
+
+export const deleteTasks = asyncWrapper(async (req, res, next) => {
+  const id = req.params.id;
+
+  // Check if the ID is a valid ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new AppError("Invalid task ID", 400);
   }
-};
+  // Await the deletion process
+  const deletedTask = await TaskList.findByIdAndDelete(id);
 
-export const deleteTasks = async (req, res) => {
-  try {
-    const id = req.params.id;
-
-    // Check if the ID is a valid ObjectId
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(400).json({ msg: "Invalid task ID" });
-    }
-    // Await the deletion process
-    const deletedTask = await TaskList.findByIdAndDelete(id);
-
-    if (!deleteTasks) {
-      return res.status(404).json({ msg: "Task not found" });
-    }
-
-    console.log("Deleted task:", deletedTask);
-    res.status(200).json({ msg: "Task deleted successfully", deletedTask });
-  } catch (error) {
-    console.error("Error deleting task:", error);
-    res.status(500).json({ msg: "Can't delete task", error: error.message });
+  if (!deletedTask) {
+    throw new NotFoundError("Task not found");
   }
-};
 
-export const updateTasks = async (req, res) => {
-  try {
-    const id = req.params.id;
-    const { task, complete } = req.body;
+  res.status(200).json({ msg: "Task deleted successfully", deletedTask });
+});
 
-    // Check if the ID is a valid ObjectId
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).json({ msg: "No task find with this id" });
-    }
+export const updateTasks = asyncWrapper(async (req, res, next) => {
+  const id = req.params.id;
+  const { task, complete } = req.body;
 
-    const updatedTask = await TaskList.findByIdAndUpdate(
-      id,
-      {
-        task,
-        complete,
-      },
-      { new: true }
-    );
-
-    if (!updatedTask) {
-      return res.status(404).json({ msg: "Task not found" });
-    }
-
-    console.log("Updated Task:", updatedTask);
-    res.status(200).json({ msg: " task updated sucessfuly", updatedTask });
-  } catch (error) {
-    res.status(500).json({ msg: "can't update task", error: error.message });
+  // Check if the ID is a valid ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new AppError("Invalid task ID", 400);
   }
-};
+
+  const updatedTask = await TaskList.findByIdAndUpdate(
+    id,
+    {
+      task,
+      complete,
+    },
+    { new: true, runValidators: true }
+  );
+
+  if (!updatedTask) {
+    throw new NotFoundError("Task not found");
+  }
+  res.status(200).json({ msg: " task updated sucessfuly", updatedTask });
+});
