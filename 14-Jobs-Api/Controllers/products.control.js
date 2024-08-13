9ca7901011298982f8c1ad6571f5ec530b171ff5
suvam <suvampravu@gmail.com>
2024-08-13 23:20:41 +0530

    commit the changes

--- 14-Jobs-Api/Controllers/products.control.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import ProductList from "../models/Task.model.js";
-import { asyncWrapper } from "../Middleware/asyncWarp.js";
-import NotFoundError from "../Error/NotFoundError.js";
-import AppError from "../Error/AppError.js";
-
--- 14-Jobs-Api/routes/products.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-// Route for products collection
-
-export default router;
