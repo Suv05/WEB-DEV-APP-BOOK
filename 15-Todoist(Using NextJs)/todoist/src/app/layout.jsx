a1719add0c8f14bc86bd80b4050be0bd0085f936
suvam <suvampravu@gmail.com>
2024-09-08 20:30:29 +0530

    Crate ui

--- 15-Todoist(Using NextJs)/todoist/src/app/layout.jsx
+++ 15-Todoist(Using NextJs)/todoist/src/app/layout.jsx
@@ -1,9 +1,15 @@
 import "./globals.css";
+import Header from "@/components/navbar/Header";
+import Footer from "@/components/footer/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header></Header>
+        {children}
+        <Footer></Footer>
+      </body>
     </html>
   );
 }
---
+++ 15-Todoist(Using NextJs)/todoist/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer({}) {
+  return (
+    <>
+      <div className="text-center py-5 bg-slate-800 text-white text-xl">
+        &copy;All rights reserved by Amsuv production House 2024
+      </div>
+    </>
+  );
+}
+
+export default Footer;
